43ba10aca1da816ab3e7a79e1f4eb87a0f1ac2a6
Doug Lavin <dml336@scarletmail.rutgers.edu>
2025-11-07 18:44:41 -0500

    Added comments

--- Doug/ad-demo/server.js
+++ Doug/ad-demo/server.js
@@ -1,10 +1,13 @@
+// Import required modules
 const express = require('express');
 const fs = require('fs');
 const csv = require('csv-parser');
 const app = express();
 
+// Serve static files (e.g., index.html) in the same directory
 app.use(express.static(__dirname));
 
+// Read CSV and pick a random video link helper function
 function getRandomVideo(callback) {
   const videos = [];
   fs.createReadStream('videos.csv')
@@ -16,12 +19,14 @@ function getRandomVideo(callback) {
     });
 }
 
+// API endpoint: Returns a random YouTube URL from CSV
 app.get('/api/video-ad', (req, res) => {
   getRandomVideo((video) => {
     res.json({ youtube_url: video.link });
   });
 });
 
+// Start the server
 app.listen(3000, () => {
   console.log('Server running on http://localhost:3000');
 });
