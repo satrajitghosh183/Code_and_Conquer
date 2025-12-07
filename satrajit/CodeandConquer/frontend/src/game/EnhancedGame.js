22e9b338e45c572dce0e015acf154295e966fda2
Satrajit Ghosh <satrajitghosh183@gmail.com>
2025-12-07 16:03:28 -0500

    Revert "Disable random floating particles that were spawning in scene"
    This reverts commit 3067b34172dc473c235201f75de99007e3880aae.

--- satrajit/CodeandConquer/frontend/src/game/EnhancedGame.js
+++ satrajit/CodeandConquer/frontend/src/game/EnhancedGame.js
@@ -870,8 +870,8 @@ export class EnhancedGame {
       this.scene.add(rock)
     }
 
-    // Floating particles (disabled - was creating random particles)
-    // this.createParticles()
+    // Floating particles
+    this.createParticles()
   }
 
   createParticles() {
